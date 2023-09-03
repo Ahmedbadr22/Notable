b85ba7ff03aa41b8645bd86805f893ae322813b4
AhmedBadr <ahmed.badr.abdeldayem.dev@gmail.com>
2023-09-03 19:07:16 +0300

    impl: splash delay then navigation in splash screen

--- app/src/main/java/com/coder/x/notable/presentation/screen/SplashFragment.java
+++ app/src/main/java/com/coder/x/notable/presentation/screen/SplashFragment.java
@@ -3,21 +3,42 @@ package com.coder.x.notable.presentation.screen;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.coder.x.notable.R;
 import com.coder.x.notable.databinding.FragmentSplashBinding;
 
 
 public class SplashFragment extends Fragment {
-    private FragmentSplashBinding binding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentSplashBinding.inflate(getLayoutInflater(), container, false);
+        FragmentSplashBinding binding = FragmentSplashBinding.inflate(getLayoutInflater(), container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        new Handler().postDelayed(this::navigateToNotesListFragment, 3000);
+    }
+
+
+    private void navigateToNotesListFragment() {
+        int navAction = R.id.action_splashFragment_to_notesListFragment;
+        NavHostFragment navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.fcv_main);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            navController.navigate(navAction);
+        }
     }
 }
